1337da2be78738e8fdf9d6b1703463f9d22fa1c2
Gabriel Oliveira <dev@gabrieloliveira.net>
2016-09-02 10:51:24 -0300

    initial commit

---
+++ server.py
@@ -0,0 +1,17 @@
+import web
+
+urls = ('/.*', 'hooks')
+
+app = web.application(urls, globals())
+
+class hooks:
+    def POST(self):
+        data = web.data()
+        print
+        print 'DATA RECEIVED:'
+        print data
+        print
+        return 'OK'
+
+if __name__ == '__main__':
+    app.run()
