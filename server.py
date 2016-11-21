547489d8aa8c841e6f5b3b51154f365abab44e1b
Gabriel Oliveira <dev@gabrieloliveira.net>
2016-11-21 17:10:36 -0200

    fix replace filename

--- server.py
+++ server.py
@@ -10,7 +10,11 @@ class hooks:
 		data = json.loads(web.data())
 		path = data['repository']['full_name'].split('/')
 		#full_name is like gabrieloliveiranet/topweb-web
-		path = os.path.join(os.path.realpath(__file__).replace('server.py', ''), 'scripts', path[0], '{}.sh'.format(path[1]))
+		current_path = os.path.realpath(__file__)
+		parts = current_path.split(os.sep)
+		del parts[-1] #delete the file name of current
+		current_path = os.sep.join(parts)
+		path = os.sep.join([current_path, 'scripts', path[0], '{}.sh'.format(path[1])])
 		try:
 			os.system(path)
 		except OSError:
