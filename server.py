828bcfe0347ff35091cdeedf801d010a4a0a761c
Gabriel Oliveira <dev@gabrieloliveira.net>
2016-11-21 12:38:17 -0200

    fix path

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ class hooks:
 		data = json.loads(web.data())
 		path = data['repository']['full_name'].split('/')
 		#full_name is like gabrieloliveiranet/topweb-web
-		path = os.path.join('.', 'scripts', path[0], '{}.sh'.format(path[1]))
+		path = os.path.join(os.path.realpath(__file__).replace('server.py', ''), 'scripts', path[0], '{}.sh'.format(path[1]))
 		try:
 			call(path)
 		except OSError:
